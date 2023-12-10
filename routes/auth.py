c4e371642608daa8ce30dcc7d0a3116d2ca1399d
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 23:10:39 +0530

    decode a JWT

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,5 @@
 from datetime import timedelta, datetime
-from jose import jwt
+from jose import jwt, JWTError
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from database import SessionLocal
@@ -13,6 +13,7 @@ from starlette import status
 SECRET_KEY = '197b2c37c391bed93fe80344fe73b806947a65e36206e05a1a23c2fa12702fe3'
 ALGORITHM = 'HS256'
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl='auth/token')
 
 
 class CreateUserRequest(BaseModel):
@@ -57,6 +58,21 @@ def create_access_token(username: str, user_id: int, role: str, expires_delta: t
     expires = datetime.utcnow() + expires_delta
     encode.update({'exp': expires})
     return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHM)
+
+
+async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get('sub')
+        user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
+        if username is None or user_id is None:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                                detail='Could not validate user.')
+        return {'username': username, 'id': user_id, 'user_role': user_role}
+    except JWTError:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
