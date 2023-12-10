22bc1406d34ffacbec2ddf22ed507484582bd683
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 20:17:16 +0530

    post method added

--- main.py
+++ main.py
@@ -6,10 +6,19 @@ import models
 from models import Todos
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
+from pydantic import BaseModel, Field
+
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+class TodoRequest(BaseModel):
+    title: str = Field(min_length=3)
+    description: str = Field(min_length=3, max_length=100)
+    priority: int = Field(gt=0, lt=6)
+    complete: bool
 
 
 def get_db():
@@ -34,3 +43,11 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
     if todo_model is not None:
         return todo_model
     raise HTTPException(status_code=404, detail='Todo not found.')
+
+
+@app.post("/todo", status_code=status.HTTP_201_CREATED)
+async def create_todo(db: db_dependency,todo_request: TodoRequest):
+    todo_model = Todos(**todo_request.model_dump())
+
+    db.add(todo_model)
+    db.commit()
