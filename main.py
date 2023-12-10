963b718148064cc9ab335ffe0cfa16a192a7c72f
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 20:13:16 +0530

    get todo with id

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException, Path
+from starlette import status
 import models
 from models import Todos
 from database import engine, SessionLocal
@@ -22,6 +23,14 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
-@app.get("/")
+@app.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
+
+
+@app.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
+async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is not None:
+        return todo_model
+    raise HTTPException(status_code=404, detail='Todo not found.')
