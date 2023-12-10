d601a2d9b46421c1d1fa7eef0b1095f67abd803d
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 20:08:49 +0530

    basic dependency injection

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from fastapi import FastAPI, Depends
 import models
-from database import engine
+from models import Todos
+from database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+@app.get("/")
+async def read_all(db: db_dependency):
+    return db.query(Todos).all()
