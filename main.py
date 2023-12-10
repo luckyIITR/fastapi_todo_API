cdfa2ca7da954335a516916511ae4744eea5d306
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 20:23:28 +0530

    put and delete added

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from database import engine, SessionLocal
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 
-
 app = FastAPI()
 
 models.Base.metadata.create_all(bind=engine)
@@ -46,8 +45,35 @@ async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
 
 @app.post("/todo", status_code=status.HTTP_201_CREATED)
-async def create_todo(db: db_dependency,todo_request: TodoRequest):
+async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo_model = Todos(**todo_request.model_dump())
 
     db.add(todo_model)
+    db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency,
+                      todo_request: TodoRequest,
+                      todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+
+    todo_model.title = todo_request.title
+    todo_model.description = todo_request.description
+    todo_model.priority = todo_request.priority
+    todo_model.complete = todo_request.complete
+
+    db.add(todo_model)
+    db.commit()
+
+
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+
     db.commit()
