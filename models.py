fb0ceebb0275ad4c1246f4f1a418b3599b51da96
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 19:53:49 +0530

    basic sqllite database setup

---
+++ models.py
@@ -0,0 +1,13 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+
+
+class Todos(Base):
+    __tablename__ = 'todos'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String)
+    description = Column(String)
+    priority = Column(Integer)
+    complete = Column(Boolean, default=False)
+    owner_id = Column(Integer)
