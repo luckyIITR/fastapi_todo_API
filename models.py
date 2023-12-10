31623665a7d327c2ea47a337cabfdc220958240c
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-10 22:23:12 +0530

    users table, create user, authenticate user

--- models.py
+++ models.py
@@ -2,6 +2,19 @@ from database import Base
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 
 
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True)
+    username = Column(String, unique=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean, default=True)
+    role = Column(String)
+
+
 class Todos(Base):
     __tablename__ = 'todos'
 
@@ -10,4 +23,4 @@ class Todos(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
-    owner_id = Column(Integer)
+    owner_id = Column(Integer, ForeignKey("users.id"))
--- routes/auth.py
+++ routes/auth.py
@@ -1,8 +1,71 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from pydantic import BaseModel
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from models import Users
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
+from passlib.context import CryptContext
+from starlette import status
+
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
 
 router = APIRouter()
 
 
-@router.get('/auth/')
-def users():
-    return {"user": "auth"}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return user
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency,
+                      create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True
+    )
+
+    db.add(create_user_model)
+    db.commit()
+
+
+@router.post("/token")
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+                                 db: db_dependency):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,
+                            detail='Could not validate user.')
+
+    return {'authenticated'}
