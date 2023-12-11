abf4c8fb0b71f1585542bd19befaa6b047f3e58b
Lucky Kushwaha <luckykushwaha56@gmail.com>
2023-12-11 12:18:34 +0530

    admin delete todo

--- routes/admin.py
+++ routes/admin.py
@@ -30,3 +30,14 @@ async def read_all(user: user_dependency, db: db_dependency):
     if user is None or user.get('user_role') != 'admin':
         raise HTTPException(status_code=401, detail='Authentication Failed')
     return db.query(Todos).all()
+
+
+@router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(user: user_dependency, db: db_dependency, todo_id: int = Path(gt=0)):
+    if user is None or user.get('user_role') != 'admin':
+        raise HTTPException(status_code=401, detail='Authentication Failed')
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
